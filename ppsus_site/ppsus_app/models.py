33022ad3a551b6b2a69d7a6713e6783437b500b4
Bruno Henrique Rasteiro <bruno.rasteiro@usp.com>
2017-11-05 18:38:11 -0200

    Models 'posto' e 'usuarios' adicionados

--- ppsus_site/ppsus_app/models.py
+++ ppsus_site/ppsus_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Posto(models.Model):
+	nome = models.CharField(max_length=100)
+	numero = models.IntegerField(blank=True)
+	endereco = models.CharField(max_length=100, blank=True)
+	bairro = models.CharField(max_length=100, blank=True)
+	cep = models.CharField(max_length=8, blank=True)
+	telefone = models.CharField(max_length=9, blank=True)
+
+class Usuario(models.Model):
+	fk_post = models.ForeignKey(Posto, on_delete=models.CASCADE, blank=True)
+	nome = models.CharField(max_length=100)
+	login = models.CharField(max_length=50)	
+	senha = models.CharField(max_length=20)
+	tipo = models.CharField(max_length=1)
+	permissao = models.IntegerField()
--- ppsus_site/ppsus_site/settings.py
+++ ppsus_site/ppsus_site/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    #'django.contrib.admin',
+    #'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'ppsus_app.apps.PpsusAppConfig'
+    #'django.contrib.sessions',
+    #'django.contrib.messages',
+    #'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ppsus_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '2194703',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432',
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
