6aa246b61180f6f0516f78132ca208d8b5495f90
Bruno Rasteiro <bruno.rasteiro@usp.br>
2018-02-19 16:36:22 -0300

    initadmin.py criando postos e admins do sistema

--- initadmin.py
+++ initadmin.py
@@ -1,25 +1,75 @@
-#from django.conf import settings
-from ppsus_app.models import User
+import django
+django.setup()
 
-#if Account.objects.count() == 0:
-#for user in settings.ADMINS:
-username = 'teste'
-password = 'bhr326126'
-#print('Creating account for %s (%s)' % (username, email))
-admin = User.objects.create_superuser(
-    username=username, 
-    password=password, 
-    first_name='Bruno', 
-    last_name='Rasteiro', 
-    posto_id=1)
-admin.is_active = True
-admin.is_admin = True
-admin.save()
+from ppsus_app.models import User, Posto
 
-print("#############")
-print("Super User criado")
-print(admin)
-print("#############")
+admins = {
+	#Aracy I
+	1: [['Aline', 'Cristina dos Santos', 'alinearacy1'],
+		['Andreá', 'Daisy dos Santos', 'andreaaracy1'],
+		['Andréia', 'Inácio de Oliveira', 'andreiaaracy1'],
+		['Lucinete', 'Alves Gomes', 'lucinetearacy1'],
+		['Priscila', 'Ribeiro Costa', 'priscilaaracy1']
+	],
+	#Aracy II
+	2: [['Ana', 'Adelaide Pereira', 'anaaracy2'],
+		['Eloana', 'Silva Teixeira', 'eloanaaracy2'],
+		['Harlem', 'Rogério Furquim', 'harlemaracy2'],
+		['Marileide', 'Cristina Pine', 'marileidearacy2'],
+		['Marli', 'de Fátima Oliveira Luiz', 'marliaracy2'],
+		['Solange', 'Volten Gobbo', 'solangearacy2']],
+	#Petrilli
+	3: [['Aparecida', 'Romualda da Cunha', 'aparecidapetrilli'],
+		['Jaquesceli', 'Carneiro de Almeida Oliveira', 'jaquescelipetrilli'],
+		['Josiane', 'Alves Pereira dos Santos', 'josianepetrilli'],
+		['Karoline', 'do Nascimento Polacci Jordão', 'karolinepetrilli'],
+		['Kelly', 'Oliveira Santos', 'kellypetrilli']],
+	#UFSCar
+	4: [['Andresa', 'Paixão', 'andresa1'],
+		['Angélica', 'Sartório', 'angelica2'],
+		['Claudete', 'de Oliveira', 'claudete3'],
+		['Fernanda', 'Karoline Generoso', 'fernanda4'],
+		['Gabriela', 'Mazzo', 'gabriela5'],
+		['Gabriella', 'Cavallaro', 'gabriella6'],
+		['Isabela', 'Machado', 'isabela7'],
+		['Juliane', 'Dias', 'juliane8'],
+		['Larissa', 'Cesário', 'larissa9'],
+		['Leticia', 'Didone', 'leticia10'],
+		['Luiz', 'Eduardo Santos', 'luizeduardo11'],
+		['Marisa', 'Silvana Zazzetta', 'marisa12'],
+		['Mirely', 'Oliveira Ghinelli', 'mirely13'],
+		['Patrícia', 'Bet', 'patricia14'],
+		['Rosângela', 'Custódio', 'rosangela15'],
+		['Ruana', 'Danieli', 'ruanadanieli16'],
+		['Tainá', 'dos Santos', 'tainasantos17'],
+		['Thainá', 'C. Duarte de Mello', 'thaina18']]}
 
-#else:
-#    print('Admin accounts can only be initialized if no Accounts exist')
+if Posto.objects.count() == 0:
+	for np in ['Aracy I', 'Aracy II', 'Petrilli', 'UFSCar']:
+		posto = Posto.objects.create(nome=np)
+		print("Posto "+np+" criado com sucesso!")	
+else:
+	print('Já existem postos cadastrados nesse banco de dados')
+
+if User.objects.count() == 0:
+	admin = User.objects.create_superuser(username='admin', password='ppsus2018', email='')
+	admin.is_active = True
+	admin.is_admin = True
+	admin.save()
+
+	for posto in admins:
+		for first_name, last_name, login in admins[posto]:
+			admin = User.objects.create_superuser(
+				username=login, 
+				password=login,
+				email='',
+				first_name=first_name, 
+				last_name=last_name, 
+				posto_id=posto)
+			admin.is_active = True
+			admin.is_admin = True
+			admin.save()
+
+			print("User "+login+" criado com sucesso!")	
+else:
+	print('Já existem usuários cadastrados nesse banco de dados')
